eaf8646bbbf409678705d085efbf09d42dbb6d4f
Mina-Luna <boettchermina05@gmail.com>
2025-10-24 12:10:40 +0200

    droid task

--- Droid/Droid.js
+++ Droid/Droid.js
@@ -2,8 +2,27 @@ export var Droid;
 (function (Droid) {
     function getCommand(_state) {
         console.log(_state);
-        return { module: "Hallo Welt", method: "BrumBrum", data: "gibdir" };
+        const command = {
+            module: "Chassis",
+            method: "move",
+            data: "forward"
+        };
+        const random = Math.floor(Math.random() * 5);
+        command.data = ["forward", "back", "left", "right", "stop"][random];
+        console.log(command.data);
+        return command;
     }
     Droid.getCommand = getCommand;
 })(Droid || (Droid = {}));
+//let count: number = 0;
+//export function getCommand(_state: object): Command {
+//console.log(_state);
+//count ++;
+//if (count %2 !== 0){
+//    return {module: "Chassis", method: "move", data: "forward"}
+//    }
+//else{
+//    return {module: "Chassis", method: "move", data: "left"}
+//    }
+//}
 //# sourceMappingURL=Droid.js.map
--- Droid/Droid.ts
+++ Droid/Droid.ts
@@ -6,10 +6,39 @@ export namespace Droid {
         data: string
     }
 
-
-
+    
+    
     export function getCommand(_state: object): Command {
+
         console.log(_state);
-        return {module: "Hallo Welt", method: "BrumBrum", data: "gibdir"}
+        const command: Command = {
+            module: "Chassis",
+            method: "move", 
+            data: "forward"
+        }
+        const random: number = Math.floor(Math.random()*5);
+
+        command.data=["forward","back","left","right","stop"][random];
+        console.log(command.data);
+
+        return command
     }
 }
+
+
+//let count: number = 0;
+
+    //export function getCommand(_state: object): Command {
+        //console.log(_state);
+        //count ++;
+        //if (count %2 !== 0){
+        //    return {module: "Chassis", method: "move", data: "forward"}
+        //    }
+        //else{
+        //    return {module: "Chassis", method: "move", data: "left"}
+        //    }
+        //}
+
+
+
+
