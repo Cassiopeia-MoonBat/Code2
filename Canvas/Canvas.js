8fcad69ffee424b82bfc3613864d0148ac916985
Mina-Luna <boettchermina05@gmail.com>
2025-10-24 16:13:00 +0200

    canvas triangular function

--- Canvas/Canvas.js
+++ Canvas/Canvas.js
@@ -5,26 +5,25 @@ var Canvas;
     function hnddraw() {
         let canvas = document.querySelector("canvas");
         let crc2 = canvas.getContext("2d");
-        canvas.width = 500;
-        canvas.height = 400;
-        crc2.fillStyle = "#159365ff";
-        crc2.fillRect(60, 60, crc2.canvas.width, crc2.canvas.height);
-        crc2.beginPath();
-        crc2.arc(100, 100, 20, 0, 1.5 * Math.PI);
-        crc2.strokeStyle = "#6f4306ff"; //color of the line/stroke
-        crc2.closePath(); //does close the circle at the angle points (straight line)
-        crc2.stroke(); //Executes the pathline
-        crc2.beginPath();
-        crc2.ellipse(70, 150, 6, 60, 5, 120, 180, false);
-        crc2.stroke(); //Executes the pathline
-        crc2.beginPath();
-        crc2.lineTo(200, 300);
-        crc2.lineTo(300, 300);
-        crc2.lineTo(300, 100);
-        crc2.lineTo(200, 300);
-        crc2.fillStyle = "#7266b1e3";
-        crc2.stroke();
-        crc2.fill();
+        canvas.width = 900;
+        canvas.height = 4000;
+        function drawTriangle(_v1, _v2, _v3, _colorfill, _colorstroke) {
+            let pathtriangle = new Path2D();
+            pathtriangle.moveTo(_v1.x1, _v1.x2);
+            pathtriangle.lineTo(_v2.x1, _v2.x2);
+            pathtriangle.lineTo(_v3.x1, _v3.x2);
+            pathtriangle.lineTo(_v1.x1, _v1.x2);
+            crc2.fillStyle = _colorfill;
+            crc2.strokeStyle = _colorstroke;
+            crc2.fill(pathtriangle);
+            crc2.stroke(pathtriangle);
+        }
+        let v1 = { x1: 10, x2: 0 };
+        let v2 = { x1: 20, x2: 3000 };
+        let v3 = { x1: 10, x2: 0 };
+        let colorfill = "#e26190ff";
+        let colorstroke = "#44138430";
+        drawTriangle(v1, v2, v3, colorfill, colorstroke);
     }
 })(Canvas || (Canvas = {}));
 //# sourceMappingURL=Canvas.js.map
